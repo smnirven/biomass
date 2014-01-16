4a1340fc415c40322b85fccb5968ebd8472149a7
Thomas Steffes <smnirven@gmail.com>
2014-01-15 22:32:47 -0500

    Remove old test file
    Signed-off-by: Thomas Steffes <smnirven@gmail.com>

--- test/biomass/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns biomass.core-test
-  (:use expectations
-        biomass.core)
-  (:import [org.joda.time DateTime]))
-
-(expect (freeze-time (DateTime. 1) (build-request {:Operation "foo"} {:AWSAccessKeyId "abcd1234" :SecretAccessKey "zyx321"}))
-        {:Signature "UOWphJGRB6sgdAuxq2/Ws0BeUoM=", :Timestamp "1969-12-31T18:00:00-06:00", :Operation "foo", :Service "AWSMechanicalTurkRequester", :Version "2012-03-25", :AWSAccessKeyId "abcd1234"})
