4f2e950b860ccca8aa67a743252b536a2ead6355
Thomas Steffes <smnirven@gmail.com>
2014-03-01 21:07:37 -0500

    Dependency updates
    * midje to 1.6.2
    * clj-http to 0.9.0
    * added exclusions to clj-http, of particular note is commons-codec
    
    Signed-off-by: Thomas Steffes <smnirven@gmail.com>

--- project.clj
+++ project.clj
@@ -6,9 +6,12 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-codec "1.0.0"]
-                 [clj-http "0.7.8"]
+                 [clj-http "0.9.0" :exclusions [cheshire
+                                                crouton
+                                                org.clojure/tools.reader
+                                                commons-codec]] ;; only this because ring-codec is behind
                  [clj-time "0.6.0"]
                  [com.github.kyleburton/clj-xpath "1.4.3"]]
-  :profiles {:dev {:dependencies [[midje "1.6.0"]]
+  :profiles {:dev {:dependencies [[midje "1.6.2"]]
                    :resource-paths ["test-resources"]
                    :plugins [[lein-midje "3.0.0"]]}})
