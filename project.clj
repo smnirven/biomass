2df0bc3273fc5c1e9d1972cde86f8afbe6d4d7c9
Shafeeq Kunnakkadan <shafeeq94@gmail.com>
2016-07-25 12:08:41 +0530

    Add some schema, bump dependenceies versions

--- project.clj
+++ project.clj
@@ -4,15 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-codec "1.0.0"]
-                 [clj-http "0.9.0" :exclusions [cheshire
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-codec "1.0.1"]
+                 [clj-http "3.1.0" :exclusions [cheshire
                                                 crouton
                                                 org.clojure/tools.reader
                                                 commons-codec]] ;; only this because ring-codec is behind
-                 [clj-time "0.7.0"]
-                 [prismatic/schema "0.2.6"]
-                 [com.github.kyleburton/clj-xpath "1.4.3"]]
+                 [clj-time "0.12.0"]
+                 [prismatic/schema "1.1.3"]
+                 [com.github.kyleburton/clj-xpath "1.4.5"]]
+  :plugins [[cider/cider-nrepl "0.12.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :resource-paths ["test-resources"]
                    :plugins [[lein-midje "3.0.0"]]}})
---
+++ src/biomass/builder/schemas.clj
@@ -0,0 +1,58 @@
+(ns ^{:author "shafeeq"
+      :doc "Contains schemas for params of various operatoins"}
+    biomass.builder.schemas
+  (:require [schema.core :as s]))
+
+
+(defonce schema-Price
+  {(s/optional-key :Amount) s/Num
+   (s/optional-key :CurrencyCode) s/Str
+   (s/optional-key :FormattedPrice) s/Str})
+
+(defonce schema-Locale
+  ;; TODO Subdivisions
+  {:Country (s/if sequential? [s/Str] s/Str)})
+
+(defonce schema-QualificationRequirement
+  {:QualificationTypeId s/Str
+   :Comparator s/Str
+   (s/optional-key :IntegerValue) (s/if sequential? [s/Int] s/Int)
+   (s/optional-key :LocaleValue) (s/if sequential? [schema-Locale] schema-Locale)
+   (s/optional-key :RequiredToPreview) s/Bool})
+
+(defonce schema-HITLayoutParameter
+  {:Name s/Str
+   :Value s/Str})
+
+(defonce schema-RegisterHITType
+  {:Operation s/Str
+   :Title s/Str
+   :Description s/Str
+   :Reward schema-Price
+   :AssignmentDurationInSeconds s/Int
+   (s/optional-key :Keywords) s/Str
+   (s/optional-key :AutoApprovalDelayInSeconds) s/Int
+   (s/optional-key :QualificationRequirement) (s/if sequential? [schema-QualificationRequirement] schema-QualificationRequirement)})
+
+
+(defonce schema-CreateHIT
+  {:Operation s/Str
+   :HITTypeId s/Str
+   (s/optional-key :Question) s/Str
+   (s/optional-key :HITLayoutId) s/Str
+   (s/optional-key :HITLayoutParameter) [schema-HITLayoutParameter]
+   :LifetimeInSeconds s/Int
+   (s/optional-key :MaxAssignments) s/Int
+   (s/optional-key :AssignmentReviewPolicy) s/Str
+   (s/optional-key :HITReviewPolicy) s/Str
+   (s/optional-key :RequesterAnnotation) s/Str
+   (s/optional-key :UniqueRequestToken) s/Str}
+)
+
+(defonce schema-notification-event-type
+  (s/enum "AssignmentAccepted" "AssignmentAbandoned" "AssignmentReturned" "AssignmentSubmitted" "AssignmentRejected" "AssignmentApproved" "HITCreated" "HITExtended" "HITDisposed" "HITReviewable" "HITExpired" "Ping"))
+
+(defonce schema-Notification
+  {:Destination s/Str
+   :Transport (s/enum "Email" "SQS")
+   :EventType (s/if sequential? [schema-notification-event-type] schema-notification-event-type)})
